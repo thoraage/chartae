c20f14d203904722222312d1ee890ac153fbea1c
Thor Åge Eldby <tel@knowit.no>
2017-11-23 23:04:37 +0100

    Support for multiple LINESTRINGs

--- components/MapToolSet.js
+++ components/MapToolSet.js
@@ -32,31 +32,59 @@ class MapToolSet extends React.Component {
     }
 
     addLayer(event) {
+        function split(str, array) {
+            str = str.trim();
+            if (!array) {
+                array = [];
+            }
+            const idx = str.search(/.LINESTRING/i);
+            if (idx === -1) {
+                if (str) {
+                    array.push(str);
+                }
+                return array;
+            }
+            const substr = str.substring(0, idx + 1).trim();
+            if (substr) {
+                array.push(substr);
+            }
+            return split(str.substring(idx + 1), array);
+        }
         const value = event.target.value;
         console.log(value);
+        const values = split(value);
+        console.log(values);
         const format = new ol.format.WKT();
-        const feature = format.readFeature(value, {
-            dataProjection:
-                'EPSG:32633',
-//                'EPSG:4326',
-            featureProjection:
-//                'EPSG:32633'
-                'EPSG:3857'
-        });
-        console.log(feature);
-        const vector = new ol.layer.Vector({
-            source: new ol.source.Vector({
+        values.forEach(value => {
+            const feature = format.readFeature(value, {
+                dataProjection:
+                    'EPSG:32633',
+                   // 'EPSG:4326',
+                   //  'EPSG:3857',
+                featureProjection:
+    //                'EPSG:32633'
+                    'EPSG:3857'
+            });
+            console.log(feature);
+            const vector = new ol.layer.Vector({
+                source: new ol.source.Vector({
                 features: [feature]
-            })
+                })
+            });
+            const style = new ol.style.Style();
+            style.pointRadius = 10;
+            const stroke = new ol.style.Stroke();
+            stroke.setColor(randomColor());
+            stroke.setWidth(5);
+            style.setStroke(stroke);
+            vector.setStyle(style);
+            this.state.map.addLayer(vector);
+            this.state.map.changed();
         });
-        let style = new ol.style.Style();
-        let stroke = new ol.style.Stroke();
-        stroke.setColor(randomColor());
-        stroke.setWidth(5);
-        style.setStroke(stroke);
-        vector.setStyle(style);
-        this.state.map.addLayer(vector);
-        this.state.map.changed();
+        // this.state.map.setView( new ol.View({
+        //     center: vector.,
+        //     zoom: 2
+        // }));
         event.target.value = '';
         this.forceUpdate();
     }
